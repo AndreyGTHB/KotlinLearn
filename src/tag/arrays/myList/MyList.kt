3c1a54e99ad68c27852b077285047275861c2394
AndreyGTHB <andr.g.tuk@gmail.com>
2020-03-30 19:42:12 +0700

    Some changes

--- src/tag/arrays/myList/MyList.kt
+++ src/tag/arrays/myList/MyList.kt
@@ -15,12 +15,12 @@ data class MyList<T> (private var value: T) {
 		}
 
     fun add(v: T) {
-        var el: MyList<T>? = this
-        while(el!!.next != null) el = el.next
+        var el: MyList<T> = this
+        while(el.next != null) el = el.next!!
         el.next = MyList<T>(v)
     }
     fun get(index: Int): T {
-        var el = this
+        var el: MyList<T> = this
         for(iEl in 0..index) if(iEl != 0) el = el.next!!
         return el.value
     }
@@ -28,26 +28,26 @@ data class MyList<T> (private var value: T) {
         var index: Int = i
         if(i > 0) index -= 1
 
-        var el: MyList<T>? = this
-        for(iEl in 0..index) if(iEl != 0) el = el!!.next
+        var el: MyList<T> = this
+        for(iEl in 0..index) if(iEl != 0) el = el.next!!
 
-        el!!.next = el.next!!.next
-        return el!!.next
+        el.next = el.next!!.next
+        return el.next
     }
-    fun removes(v: T): Array<Int> {
-        var el: MyList<T>? = this
-        var indices: Array<Int> = emptyArray()
+    fun removes(v: T): Int {
+        var el: MyList<T> = this
+        var values: Int = 0
 
         var i: Int = 0
-        while(el!!.next != null) {
-            el = el.next
-            if(el!!.value == v) {
-                indices += i
+        while(el.next != null) {
+            el = el.next!!
+            if(el.value == v) {
+                values += i
                 remove(i)
             }
             i ++
         }
 
-        return indices
+        return values
     }
 }
